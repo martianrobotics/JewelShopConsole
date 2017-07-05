d42bbc92138eba702590c84b5bd279f6565f2582
Jupiter Arias <jupiter.arias@gmail.com>
2017-07-05 07:30:06 -0400

    Add project files.

---
+++ JewelShopConsole/JewelThief.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JewelShopConsole
+{
+    class JewelThief : Locksmith
+    {
+        private Jewels stolenJewels = null;
+        
+        public override void ReturnContents(Jewels safeContents, Owner owner)
+        //new public void ReturnContents(Jewels safeContents, Owner owner)
+        {
+            stolenJewels = safeContents;
+            Console.WriteLine("I'm stealing the contents! " + stolenJewels.Sparkle());
+        }
+    }
+}
---
+++ JewelShopConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JewelShopConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Owner owner = new Owner();
+            Safe safe = new Safe();
+
+            JewelThief jewelThief = new JewelThief();
+            jewelThief.OpenSafe(safe, owner);
+            
+            Locksmith calledAsLocksmith = new Locksmith();
+            calledAsLocksmith.OpenSafe(safe, owner);
+
+            Console.ReadKey();
+        }
+    }
+}
